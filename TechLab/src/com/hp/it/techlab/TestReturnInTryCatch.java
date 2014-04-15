8deb6654e321ba4ba5e85ae8409dfcb35e143e64
rogerZhang <zhangyonghuifang@163.com>
2014-04-15 22:05:38 +0800

    format them

--- TechLab/src/com/hp/it/techlab/TestReturnInTryCatch.java
+++ TechLab/src/com/hp/it/techlab/TestReturnInTryCatch.java
@@ -5,18 +5,21 @@ public class TestReturnInTryCatch {
 	
 	public static void main(String[] args){
 		int i = returnValue();
-		System.out.println(i);
+		System.out.println("returnValue i=" + i);
 	}
 	
 	private static int returnValue(){
 		try{
-			throw new Exception();
+			//throw new Exception();
+			int j = 0;
+			//int a = i/j;
+			return ++i;
 		} catch(Exception e){
 			return ++i;
 		} finally{
 			++i;
-			System.out.println("returnValue method" + i);
-			return i;
+			System.out.println("finally block i=" + i);
+			//return i;
 		}
 	}
 }
