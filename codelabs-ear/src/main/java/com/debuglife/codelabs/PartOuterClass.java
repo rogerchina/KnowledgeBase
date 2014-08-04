8d5cad297763da065797631c414238e3e5de1b30
rogerZhang <zhangyonghuifang@163.com>
2014-08-04 16:49:08 +0800

    add a sub project to package the codelabs project.

---
+++ codelabs-ear/src/main/java/com/debuglife/codelabs/PartOuterClass.java
@@ -0,0 +1,18 @@
+package com.debuglife.codelabs;
+
+public abstract class PartOuterClass {
+	private int i = 10;
+	public abstract int addValue();
+
+	public static void main(String[] args){
+		System.out.println(new PartOuterClass(){
+			int j;
+			@Override
+			public int addValue() {
+				j++;
+				return 0;
+			}
+			
+		});
+	}
+}
---
+++ codelabs-ear/src/main/java/com/debuglife/codelabs/TestThrowable.java
@@ -0,0 +1,5 @@
+package com.debuglife.codelabs;
+
+public class TestThrowable {
+
+}
